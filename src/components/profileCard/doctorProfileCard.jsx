057f81d2e30ae9feb42cdfb35cf400c6cbda539a
plaze <banikakash131@gamil.com>
2023-10-03 16:50:07 +0530

    feat: countdown timer added

--- src/components/profileCard/doctorProfileCard.jsx
+++ src/components/profileCard/doctorProfileCard.jsx
@@ -1,7 +1,22 @@
 import "./styles.css";
 import profile_pic from "./Assest/101.jpg";
+import { useState, useRef, useEffect } from "react";
 
 const DoctorProfileCard = ({ name, description, image, totalTime, type }) => {
+  const [time, setTime] = useState(totalTime);
+  const timeId = useRef();
+
+  useEffect(() => {
+    timeId.current = setInterval(() => {
+      setTime((prev) => prev - 1);
+    }, 1000);
+    return () => clearInterval(timeId.current);
+  }, []);
+  useEffect(() => {
+    if (time <= 0) {
+      clearInterval(timeId.current);
+    }
+  }, [time]);
   const src = `${profile_pic}/../${image}.jpg`;
   //console.log(src);
   return (
@@ -11,7 +26,7 @@ const DoctorProfileCard = ({ name, description, image, totalTime, type }) => {
         <img src={profile_pic} alt="Doctor" />
         <h2 className="profile-title">{name}</h2>
         <p className="profile-descrption">{`${type} ${description}`}</p>
-        <button className="profile-time">{`Waiting Time:${totalTime} min for next appointment`}</button>
+        <button className="profile-time">{`Waiting Time:${time} min for next appointment`}</button>
         <button className="profile-button">
           <a href="https://github.dev/0plaze0">Contact</a>
         </button>
